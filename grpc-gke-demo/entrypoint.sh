f7feca90cfdd3973f8b36795f4cff9a81c7ed416
chaoshwu <chaoshanwu@gmail.com>
2025-04-14 23:05:20 -0400

    update entrypoint.sh

--- grpc-gke-demo/entrypoint.sh
+++ grpc-gke-demo/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 python3 -m grpc_tools.protoc -I./proto --python_out=./proto --grpc_python_out=./proto ./proto/cluster_service.proto
+export PYTHONPATH=$PYTHONPATH:/app
 python3 server/main.py
